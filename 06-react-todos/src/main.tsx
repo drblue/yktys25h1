fcb78e03c9edfa784832e74878d53b48277b69c2
Johan Nordström <johan@digitalvillage.se>
2025-12-17 19:56:16 +0100

    Increased stale-time 👴🏻 and garbage collection-time 🗑️.

--- 06-react-todos/src/main.tsx
+++ 06-react-todos/src/main.tsx
@@ -6,7 +6,15 @@ import ThemeProvider from "./contexts/ThemeProvider.tsx";
 import App from "./App.tsx";
 
 // Create the query client
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			staleTime: 5 * 60 * 1000,  // 5 minutes
+			gcTime: 15 * 60 * 1000,  // 15 minutes
+		}
+	}
+});
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
